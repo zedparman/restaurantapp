d6566ef0a02d7226afc62933d4cbf1e099cc7710
zedparman <58415963+zedparman@users.noreply.github.com>
2021-06-17 11:24:08 -0700

    Update index.js

--- store/index.js
+++ store/index.js
@@ -17,9 +17,10 @@ export const mutations = {
 export const actions = {
     setRestaurant: async ({ commit }, payload) => {
         try {
-            let result = await axios.get('http://denj.ichimoku-fibo.ir/menu?servicePlaceId=1') 
+            // let result = await axios.get('http://denj.ichimoku-fibo.ir/menu?servicePlaceId=1') 
+            let result = await axios.get('https://jsonplaceholder.typicode.com/posts') 
             commit('SET_RESTAURANT', result.data)
-            console.log('result')
+            console.log(result)
         } catch (error) {
             console.log(error)
         }
