877d3d3e479d1678d669f1643f8e882168c1132e
zedparman <58415963+zedparman@users.noreply.github.com>
2021-06-17 13:27:34 -0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,59 @@
 <template>
   <div>
     <h1>Hello world</h1>
-    <div v-for="restaurant in restaurantList" :key="restaurant.servicePlaceId">{{ restaurant }}</div>
+    <!-- <div v-for="restaurant in restaurantList" :key="restaurant.servicePlaceId">{{ restaurant }}</div> -->
+    <div v-for="todo in allTodos" :key="todo.id">{{ todo.title }}</div>
+
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+// import { mapState, mapActions } from 'vuex';
+
+// export default {
+//   methods: {
+//     ...mapActions([
+//       'setRestaurant', //also supports payload `this.nameOfAction(amount)` 
+//     ])
+//   },
+//   computed: {
+//     ...mapState([
+//       'restaurantList'
+//     ])
+//   }
+
+// //   computed: {
+// // restaurantList() {
+// // return this.$store.state.restaurantList;
+// // }
+// // },
+// // mounted() {
+// // this.$store.dispatch('setRestaurant');
+// // }
+
+// };
+
+
+
+
+
+
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  methods: {
-    ...mapActions([
-      'setRestaurant', //also supports payload `this.nameOfAction(amount)` 
-    ])
-  },
-  computed: {
-    ...mapState([
-      'restaurantList'
-    ])
-  }
+name: "Todos",
+methods: {
+...mapActions(['fetchTodos'])
+},
+computed: mapGetters(["allTodos"]),
 
-//   computed: {
-// restaurantList() {
-// return this.$store.state.restaurantList;
-// }
-// },
-// mounted() {
-// this.$store.dispatch('setRestaurant');
-// }
+created() {
+  this.fetchTodos();
+}
 
-};
+  };
+
+
 </script>
 
 <style lang="scss" scoped>
