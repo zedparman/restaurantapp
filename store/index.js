52f81b38fa13d393cdb547ff0c111ecb5ab532b0
zedparman <58415963+zedparman@users.noreply.github.com>
2021-06-17 13:27:37 -0700

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-    restaurantList: []
+    todos: []
 })
 
 // export const getters = {
@@ -8,21 +8,42 @@ export const state = () => ({
 //     }
 // }
 
-export const mutations = {
-    SET_RESTAURANT: (state, payload) => {
-        state.restaurantList = payload
-    }
-}
+// export const mutations = {
+//     SET_RESTAURANT: (state, payload) => {
+//         state.payload = payload
+//     }
+// }
 
-export const actions = {
-    setRestaurant: async ({ commit }, payload) => {
-        try {
-            // let result = await axios.get('http://denj.ichimoku-fibo.ir/menu?servicePlaceId=1') 
-            let result = await axios.get('https://jsonplaceholder.typicode.com/posts') 
-            commit('SET_RESTAURANT', result.data)
-            console.log(result)
-        } catch (error) {
-            console.log(error)
-        }
+// export const actions = {
+//     setRestaurant: async ({ commit }) => {
+//         try {
+//             // let result = await axios.get('http://denj.ichimoku-fibo.ir/menu?servicePlaceId=1') 
+//             let result = await axios.get('https://jsonplaceholder.typicode.com/todos') 
+//             commit('SET_RESTAURANT', result.data)
+//             console.log(result)
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+// }
+
+
+const getters = {
+    allTodos: state => state.todos
+    };
+    
+
+const actions = {
+    async fetchTodos({ commit }) {
+    const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/todos'
+    );
+    
+    commit('setTodos', response.data);
     }
-}
+    };
+    
+    const mutations = {
+    setTodos: (state, todos) => (state.todos = todos)
+    };
+    
