af4151843bdc9b3ed8a72a7e7a28e9d331996547
zedparman <58415963+zedparman@users.noreply.github.com>
2021-06-17 08:58:02 -0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Hello world</h1>
+    <div v-for="restaurant in restaurantList" :key="restaurant.id">{{ restaurant }}</div>
   </div>
 </template>
 
 <script>
   export default {
-    
+    computed: {
+      restaurantList() {
+        console.log(this.$store.state.restaurantList)
+        return this.$store.state.restaurantList; 
+      }
+    },
   }
 </script>
 
