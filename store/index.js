8b9b6bb5e84e9dda75ff976c3b9a637754645ee8
zedparman <58415963+zedparman@users.noreply.github.com>
2021-06-17 08:58:04 -0700

    Create index.js

---
+++ store/index.js
@@ -0,0 +1,26 @@
+export const state = () => ({
+    restaurantList: [
+        {
+            "id": 1,
+            "name": "heyyy"
+        }
+    ]
+})
+
+// export const getters = {
+//     getterValue: state => {
+//         return state.value
+//     }
+// }
+
+// export const mutations = {
+//     updateValue: (state, payload) => {
+//         state.value = payload
+//     }
+// }
+
+// export const actions = {
+//     updateActionValue({ commit }) {
+//         commit('updateValue', payload)
+//     }
+// }
