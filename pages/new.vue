16107d6c101a86038b472699bc8052cbbdd3885d
zedparman <58415963+zedparman@users.noreply.github.com>
2021-06-18 21:22:52 -0700

    Create new.vue

---
+++ pages/new.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="countwr">
+            0
+        </div>
+        <div class="buttons">
+            <button>-</button>
+            <button>+</button>
+        </div>
+        <v-btn>f</v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+div {
+    margin-bottom: 10px;
+
+}
+.counter{
+    font-size: 80px;
+}
+.buttons button{
+    font-size: 40px;
+}
+</style>
